50e6a279030f7bdf7ecf36d36ea44e85ac2abe9c
ybowang <ybowang@thoughtworks.com>
2024-12-05 21:24:10 +0800

    [Yuanbo] #N/A add video compress

--- frontend/video-compress/script.js
+++ frontend/video-compress/script.js
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }, 200);
 
-            const response = await fetch('http:localhost:8000/api/video/compress', {
+            const response = await fetch('https://tools-as5l.onrender.com/api/video/compress', {
                 method: 'POST',
                 body: formData
             });
